26d25a315437c9e2d62f6953e67e67b1f8e54589
cmgrimm <cmgrimm@ccanet.rockwellcollins.com>
2016-09-12 11:46:38 -0500

    Added R code
    This code contains the function in R code.

---
+++ RPackage_update.R
@@ -0,0 +1,11 @@
+packages <- c(
+  # Packages here
+)
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+ipak(packages)
